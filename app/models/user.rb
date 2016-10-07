7dde254ae22626d69146a29407c4b7ce8110ded9
Diego Bonfim <diego.bonfim@moip.com.br>
2016-10-07 00:30:37 -0300

    Add edit and show feature do user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	validates_presence_of :email, :full_name, :location, :password
+	validates_confirmation_of :password
+	validates_length_of :bio, minimum:1, allow_blanck:false
+	validates_uniqueness_of :email
 end
