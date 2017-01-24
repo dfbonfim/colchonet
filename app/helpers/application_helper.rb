253e68a40df89d91074884a18009d3ec9c66e70c
Diego Freitas <diego.bonfim@moip.com.br>
2017-01-24 00:03:18 -0200

    helper to show tag with errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def error_tag(model, attribute)
+    if model.errors.has_key? attribute
+      content_tag :div, model.errors[attribute].first,
+                  :class => 'error_message'
+    end
+  end
+
+  def check_form_errors(model)
+    if model.errors.any?
+      content_tag :div, "Há erros no formulário, por favor verifique.",
+                  :id => 'error_explanation'
+    end
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ApplicationRecord
 
+  validates_presence_of :title, :location, :description
+  validates_length_of :description, minimum:3, allow_blanck:false
+
   def complete_name
     "#{title}, #{location}"
   end
