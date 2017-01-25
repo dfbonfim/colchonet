288be3234bd35660308115a6415218445f03bf79
Diego Freitas <diego.bonfim@moip.com.br>
2017-01-24 23:26:40 -0200

    Set locale on endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  LOCALES = /en|pt/
+
+  scope "(:locale)", :locale => LOCALES do
+    resources :rooms
+    resources :users
+  end
+
+  get '/:locale' => 'home#index', :locale => LOCALES
   root 'home#index'
 end
